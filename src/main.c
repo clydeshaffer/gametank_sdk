af5745b77a9c8c8b9b690589f24c1d73e01edadf
Nick Girardo <nickgirardo@gmail.com>
2023-10-31 21:36:43 -0400

    Split out some of the logic from main

--- src/main.c
+++ src/main.c
@@ -149,6 +149,212 @@ char undo_buffer_get() {
     return undo_buffer[current_undo_slot + 1];
 }
 
+void main_menu_loop() {
+    clear_screen(243);
+    draw_sprite(0,0,127,127,0,0,2);
+    draw_sprite_frame(&ASSET__gfx__tinychars_json,
+    64, 104,(player_select << 2) + 2 + ((tick & 8) >> 3), 0, 0);
+    await_draw_queue();
+    sleep(1);
+    flip_pages();
+    update_inputs();
+    tick_music();
+    ++tick;
+    if(player1_buttons & ~player1_old_buttons & INPUT_MASK_LEFT) {
+        --player_select;
+        if(player_select == 0xFF) {
+            player_select = 5;
+        }
+    }
+    if(player1_buttons & ~player1_old_buttons & INPUT_MASK_RIGHT) {
+        player_select = (player_select+1)%6;
+    }
+}
+
+void game_loop() {
+    char attempted_move_dir = 0;
+    char attempt_undo = 0;
+
+    clear_screen(243);
+    if(anim_timer) {
+        anim_frame = 2 + ((anim_timer & 4) >> 2);
+        if((tick & 1) || (player1_buttons & INPUT_MASK_B)) {
+            offset_x += move_x;
+            offset_y += move_y;
+            --anim_timer;
+        }
+        // Have we just now finished our animation?
+        if(!anim_timer) {
+            player_x += move_x;
+            player_y += move_y;
+            if(pushing_box) {
+                i = (player_x + move_x) | ((player_y + move_y) << 4);
+                if(field[i] == GOAL_TILE) {
+                    field[i] = BARREL_GOAL_TILE;
+                }
+                else {
+                    field[i] = BARREL_TILE;
+                }
+                pushing_box = 0;
+            }
+            // TODO lazy copy and paste
+            if(pulling_box) {
+                i = (player_x - move_x) | ((player_y - move_y) << 4);
+                if(field[i] == GOAL_TILE) {
+                    field[i] = BARREL_GOAL_TILE;
+                }
+                else {
+                    field[i] = BARREL_TILE;
+                }
+                pulling_box = 0;
+            }
+            move_x = 0;
+            move_y = 0;
+            offset_x = DEFAULT_OFFSET;
+            offset_y = DEFAULT_OFFSET;
+        }
+    } else {
+        anim_frame = ((tick & 32) >> 5);
+    }
+    anim_frame += anim_dir;
+    anim_frame += player_select << 2;
+
+    update_inputs();
+
+    if(anim_timer == 0) {
+        if(player1_buttons & INPUT_MASK_LEFT) {
+            attempted_move_dir = move_left;
+            move_x = 0xFF;
+            anim_timer = ANIM_TIME;
+            anim_dir = 0;
+            anim_flip = 0;
+        } else if(player1_buttons & INPUT_MASK_RIGHT) {
+            attempted_move_dir = move_right;
+            move_x = 1;
+            anim_timer = ANIM_TIME;
+            anim_dir = 0;
+            anim_flip = SPRITE_FLIP_X;
+        } else if(player1_buttons & INPUT_MASK_UP) {
+            attempted_move_dir = move_up;
+            move_y = 0xFF;
+            anim_timer = ANIM_TIME;
+            anim_dir = 48;
+            anim_flip = 0;
+        } else if(player1_buttons & INPUT_MASK_DOWN) {
+            attempted_move_dir = move_down;
+            move_y = 1;
+            anim_timer = ANIM_TIME;
+            anim_dir = 24;
+            anim_flip = 0;
+        } else if(player1_buttons & INPUT_MASK_C) {
+            anim_timer = ANIM_TIME;
+        }
+
+        if(anim_timer == ANIM_TIME) {
+            if(player1_buttons & INPUT_MASK_C) {
+                char last_move = undo_buffer_get();
+                if (last_move != 0xFF) {
+                    // Reverse movement direction
+                    if ((last_move & MOVE_MASK) == move_left)
+                        move_x = 1;
+                    else if ((last_move & MOVE_MASK) == move_right)
+                        move_x = 0xFF;
+                    else if ((last_move & MOVE_MASK) == move_up)
+                        move_y = 1;
+                    else if ((last_move & MOVE_MASK) == move_down)
+                        move_y = 0xFF;
+
+                    // If the barrel was moved take care of that
+                    if (last_move & barrel_was_pushed) {
+                        i = (player_x - move_x) | ((player_y - move_y) << 4);
+                        move_barrel_off_of(i);
+                        pulling_box = 1;
+                    }
+                }
+            } else {
+                i = (player_x + move_x) | ((player_y + move_y) << 4);
+                if(field[i] & 128) {
+                    // Attempting to push a barrel
+                    if((field[i] == BARREL_TILE) || (field[i] == BARREL_GOAL_TILE)) {
+                        // Target tile for barrel
+                        c = (player_x + move_x + move_x) | ((player_y + move_y + move_y) << 4);
+                        if(field[c] & 128) {
+                            // Attempting to push a barrel into a wall or another barrel
+                            move_x = 0;
+                            move_y = 0;
+                            anim_timer = 0;
+                        } else {
+                            // Move and push barrel
+                            undo_buffer_set(attempted_move_dir | barrel_was_pushed);
+                            pushing_box = 1;
+                            do_noise_effect(64, 0xFF, ANIM_TIME);
+                            move_barrel_off_of(i);
+                        }
+                    } else {
+                        // Attempting to move into a wall
+                        move_x = 0;
+                        move_y = 0;
+                        anim_timer = 0;
+                    }
+                } else {
+                    // Simple move
+                    undo_buffer_set(attempted_move_dir | barrel_wasnt_pushed);
+                    do_noise_effect(80,0,1);
+                }
+            }
+        }
+    }
+
+    while(queue_pending != 0) {
+        asm("CLI");
+        wait();
+    }
+    draw_field();
+
+    // Check for level completion
+    // I made this slower so that I didn't have to keep track of how many
+    // goals were remaining lol
+    for (i = 0; i < FIELD_SIZE; i++) {
+        // If we find any GOAL_TILEs the level isn't solved
+        // Otherwise they would be BARREL_GOAL_TILEs
+        if (field[i] == GOAL_TILE) break;
+        // We've finished looking for GOAL_TILEs and haven't found any
+        // The level is complete!
+        if (i == FIELD_SIZE - 1) {
+            load_next_level();
+            play_song(&ASSET__mid__solve_mid, REPEAT_NONE);
+        }
+    }
+
+    if(player1_buttons & ~player1_old_buttons & INPUT_MASK_START) {
+        next_level = current_level;
+        load_next_level();
+        play_song(&ASSET__mid__oops_mid, REPEAT_NONE);
+    }
+
+    if(pushing_box) {
+        draw_sprite_now((player_x << 3) + offset_x + (move_x << 3) - DEFAULT_OFFSET,
+            (player_y << 3) + offset_y + (move_y << 3) - DEFAULT_OFFSET, 8, 8, 16, 24, 1);
+    }
+
+    if(pulling_box) {
+        draw_sprite_now((player_x << 3) + offset_x - (move_x << 3) - DEFAULT_OFFSET,
+            (player_y << 3) + offset_y - (move_y << 3) - DEFAULT_OFFSET, 8, 8, 16, 24, 1);
+    }
+
+    clear_border(0);
+    draw_sprite_frame(
+        &ASSET__gfx__tinychars_json,
+        (player_x << 3) + offset_x,
+        (player_y << 3) + offset_y,
+        anim_frame, anim_flip,
+        0);
+    sleep(1);
+    flip_pages();
+    tick_music();
+    ++tick;
+}
+
 int main () {
 
     init_graphics();
@@ -179,221 +385,16 @@ int main () {
 
     // Start menu
     while(!(player1_buttons & INPUT_MASK_START)) {
-        clear_screen(243);
-        draw_sprite(0,0,127,127,0,0,2);
-        draw_sprite_frame(&ASSET__gfx__tinychars_json,
-        64, 104,(player_select << 2) + 2 + ((tick & 8) >> 3), 0, 0);
-        await_draw_queue();
-        sleep(1);
-        flip_pages();
-        update_inputs();
-        tick_music();
-        ++tick;
-        if(player1_buttons & ~player1_old_buttons & INPUT_MASK_LEFT) {
-            --player_select;
-            if(player_select == 0xFF) {
-                player_select = 5;
-            }
-        }
-        if(player1_buttons & ~player1_old_buttons & INPUT_MASK_RIGHT) {
-            player_select = (player_select+1)%6;
-        }
+        main_menu_loop();
     }
 
     stop_music();
 
     // Main game loop, run forever
     while (1) {
-        char attempted_move_dir = 0;
-        char attempt_undo = 0;
-
-        clear_screen(243);
-        if(anim_timer) {
-            anim_frame = 2 + ((anim_timer & 4) >> 2);
-            if((tick & 1) || (player1_buttons & INPUT_MASK_B)) {
-                offset_x += move_x;
-                offset_y += move_y;
-                --anim_timer;
-            }
-            // Have we just now finished our animation?
-            if(!anim_timer) {
-                player_x += move_x;
-                player_y += move_y;
-                if(pushing_box) {
-                    i = (player_x + move_x) | ((player_y + move_y) << 4);
-                    if(field[i] == GOAL_TILE) {
-                        field[i] = BARREL_GOAL_TILE;
-                    }
-                    else {
-                        field[i] = BARREL_TILE;
-                    }
-                    pushing_box = 0;
-                }
-                // TODO lazy copy and paste
-                if(pulling_box) {
-                    i = (player_x - move_x) | ((player_y - move_y) << 4);
-                    if(field[i] == GOAL_TILE) {
-                        field[i] = BARREL_GOAL_TILE;
-                    }
-                    else {
-                        field[i] = BARREL_TILE;
-                    }
-                    pulling_box = 0;
-                }
-                move_x = 0;
-                move_y = 0;
-                offset_x = DEFAULT_OFFSET;
-                offset_y = DEFAULT_OFFSET;
-            }
-        } else {
-            anim_frame = ((tick & 32) >> 5);
-        }
-
-        anim_frame += anim_dir;
-        anim_frame += player_select << 2;
-
-        update_inputs();
-
-        if(anim_timer == 0) {
-            if(player1_buttons & INPUT_MASK_LEFT) {
-                attempted_move_dir = move_left;
-                move_x = 0xFF;
-                anim_timer = ANIM_TIME;
-                anim_dir = 0;
-                anim_flip = 0;
-            }
-            else if(player1_buttons & INPUT_MASK_RIGHT) {
-                attempted_move_dir = move_right;
-                move_x = 1;
-                anim_timer = ANIM_TIME;
-                anim_dir = 0;
-                anim_flip = SPRITE_FLIP_X;
-            }
-            else if(player1_buttons & INPUT_MASK_UP) {
-                attempted_move_dir = move_up;
-                move_y = 0xFF;
-                anim_timer = ANIM_TIME;
-                anim_dir = 48;
-                anim_flip = 0;
-            }
-            else if(player1_buttons & INPUT_MASK_DOWN) {
-                attempted_move_dir = move_down;
-                move_y = 1;
-                anim_timer = ANIM_TIME;
-                anim_dir = 24;
-                anim_flip = 0;
-            }
-            else if(player1_buttons & INPUT_MASK_C) {
-                anim_timer = ANIM_TIME;
-            }
-
-            if(anim_timer == ANIM_TIME) {
-                if(player1_buttons & INPUT_MASK_C) {
-                    char last_move = undo_buffer_get();
-                    if (last_move != 0xFF) {
-                        // Reverse movement direction
-                        if ((last_move & MOVE_MASK) == move_left) {
-                            move_x = 1;
-                        }
-                        else if ((last_move & MOVE_MASK) == move_right) {
-                            move_x = 0xFF;
-                        }
-                        else if ((last_move & MOVE_MASK) == move_up) {
-                            move_y = 1;
-                        }
-                        else if ((last_move & MOVE_MASK) == move_down) {
-                            move_y = 0xFF;
-                        }
-
-                        // If the barrel was moved take care of that
-                        if (last_move & barrel_was_pushed) {
-                            i = (player_x - move_x) | ((player_y - move_y) << 4);
-                            move_barrel_off_of(i);
-                            pulling_box = 1;
-                        }
-                    }
-                }
-                else {
-                    i = (player_x + move_x) | ((player_y + move_y) << 4);
-                    if(field[i] & 128) {
-                        // Attempting to push a barrel
-                        if((field[i] == BARREL_TILE) || (field[i] == BARREL_GOAL_TILE)) {
-                            // Target tile for barrel
-                            c = (player_x + move_x + move_x) | ((player_y + move_y + move_y) << 4);
-                            if(field[c] & 128) {
-                                // Attempting to push a barrel into a wall or another barrel
-                                move_x = 0;
-                                move_y = 0;
-                                anim_timer = 0;
-                            } else {
-                                // Move and push barrel
-                                undo_buffer_set(attempted_move_dir | barrel_was_pushed);
-                                pushing_box = 1;
-                                do_noise_effect(64, 0xFF, ANIM_TIME);
-                                move_barrel_off_of(i);
-                            }
-                        } else {
-                            // Attempting to move into a wall
-                            move_x = 0;
-                            move_y = 0;
-                            anim_timer = 0;
-                        }
-                    } else {
-                        // Simple move
-                        undo_buffer_set(attempted_move_dir | barrel_wasnt_pushed);
-                        do_noise_effect(80,0,1);
-                    }
-                }
-            }
-        }
-
-        while(queue_pending != 0) {
-            asm("CLI");
-            wait();
-        }
-        draw_field();
-
-        // Check for level completion
-        // I made this slower so that I didn't have to keep track of how many
-        // goals were remaining lol
-        for (i = 0; i < FIELD_SIZE; i++) {
-            // If we find any GOAL_TILEs the level isn't solved
-            // Otherwise they would be BARREL_GOAL_TILEs
-            if (field[i] == GOAL_TILE) break;
-            // We've finished looking for GOAL_TILEs and haven't found any
-            // The level is complete!
-            if (i == FIELD_SIZE - 1) {
-                load_next_level();
-                play_song(&ASSET__mid__solve_mid, REPEAT_NONE);
-            }
-        }
-
-        if(player1_buttons & ~player1_old_buttons & INPUT_MASK_START) {
-            next_level = current_level;
-            load_next_level();
-            play_song(&ASSET__mid__oops_mid, REPEAT_NONE);
-        }
-
-        if(pushing_box) {
-            draw_sprite_now((player_x << 3) + offset_x + (move_x << 3) - DEFAULT_OFFSET,
-                (player_y << 3) + offset_y + (move_y << 3) - DEFAULT_OFFSET, 8, 8, 16, 24, 1);
-        }
-
-        if(pulling_box) {
-            draw_sprite_now((player_x << 3) + offset_x - (move_x << 3) - DEFAULT_OFFSET,
-                (player_y << 3) + offset_y - (move_y << 3) - DEFAULT_OFFSET, 8, 8, 16, 24, 1);
-        }
-
-        clear_border(0);
-        draw_sprite_frame(&ASSET__gfx__tinychars_json,
-        (player_x << 3) + offset_x,
-        (player_y << 3) + offset_y,
-        anim_frame, anim_flip, 0);
-        sleep(1);
-        flip_pages();
-        tick_music();
-        ++tick;
+        game_loop();
     }
 
-  return (0);                                     //  We should never get here!
+  // We should never get here!
+  return (0);
 }
