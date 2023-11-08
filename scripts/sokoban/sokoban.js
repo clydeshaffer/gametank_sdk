9ab9efb54cdd38e169691dbc98991315fbc5d0a7
Nick Girardo <nickgirardo@gmail.com>
2023-11-08 01:20:02 -0500

    Rewrote sokoban conversion script

--- scripts/sokoban/sokoban.js
+++ scripts/sokoban/sokoban.js
@@ -1,120 +1,174 @@
-const fs = require('fs');
-const xml2js = require('xml2js');
-const zlib = require('zlib');
+const fs = require("fs");
+const xml2js = require("xml2js");
+const zlib = require("zlib");
+
+const BANK_SIZE = 16384;
+
+const CHAR_0 = 168;
+const CHAR_1 = 169;
+const CHAR_2 = 170;
+const CHAR_3 = 171;
+const CHAR_4 = 172;
+const CHAR_5 = 173;
+const CHAR_6 = 174;
+const CHAR_7 = 175;
+const CHAR_8 = 184;
+const CHAR_9 = 185;
+
+const PLAYER_START = 32;
+const PLAYER_GOAL_START = 24;
+const TILE_GOAL = 16;
+const TILE_BARREL = 252;
+const TILE_GOAL_BARREL = 253;
+const TILE_WALL = 166;
 
 const parser = new xml2js.Parser();
 
-const argv = require('minimist')(process.argv.slice(2));
+const argv = require("minimist")(process.argv.slice(2));
 const inFileName = argv._[0];
-const outFileName = argv._.length == 2 ?
-    argv._[1] :
-    inFileName.split(".").slice(0, -1).join(".") + ".bin";
+const outFileName =
+  argv._.length == 2
+    ? argv._[1]
+    : inFileName.split(".").slice(0, -1).join(".") + ".bin";
 
-function writeNumberInMiddle(targetStr, srcStr) {
-    const header = targetStr.slice(0,-(16 + srcStr.length));
-    const footer = targetStr.slice(-16);
-    return header+srcStr+footer;
+function writeLevelNumber(targetStr, srcStr) {
+  const header = targetStr.slice(0, -(16 + srcStr.length));
+  const footer = targetStr.slice(-16);
+  return header + srcStr + footer;
 }
 
-fs.readFile(inFileName, function(err, data) {
-    parser.parseString(data, (xmlErr, jsonObj) => {
-        const levels = jsonObj.SokobanLevels.LevelCollection[0].Level;
-        let totalLevels = 0;
-        let totalSize = 0;
-        const convertedLevels = levels.flatMap((level) => {
-            //console.log(level);
-            if(totalSize < 16300) { 
-                if(level.$.Width <= 16 && level.$.Height <= 14) {
-                    ++totalLevels;
-                    const widthDiff = 16 - level.$.Width;
-                    const heightDiff = 16 - level.$.Height;
-                    const leftPad = Math.floor(widthDiff/2);
+const range = (n) => Array(n).keys();
 
-                    const padStringLeft = "".padStart(leftPad, " ");
+const getTileFromChar = (c) => {
+  switch (c) {
+    case "@":
+    case "p":
+      return PLAYER_START;
+    case "#":
+      return TILE_WALL;
+    case ".":
+      return TILE_GOAL;
+    case "$":
+      return TILE_BARREL;
+    case "B":
+    case "*":
+      return TILE_GOAL_BARREL;
+    case "P":
+    case "+":
+      return PLAYER_GOAL_START;
+    case "0":
+      return CHAR_0;
+    case "1":
+      return CHAR_1;
+    case "2":
+      return CHAR_2;
+    case "3":
+      return CHAR_3;
+    case "4":
+      return CHAR_4;
+    case "5":
+      return CHAR_5;
+    case "6":
+      return CHAR_6;
+    case "7":
+      return CHAR_7;
+    case "8":
+      return CHAR_8;
+    case "9":
+      return CHAR_9;
+    case " ":
+      // Random foliage
+      // TODO the rands here make builds non-reproduceable lol
+      // Consider replacing with a prng, e.g. Wichmann-Hill, to seed consistently
+      return Math.random() > 0.75 ? Math.floor(Math.random() * 5) + 64 : 0;
+    default:
+      throw new Error(`unexpected tile: ${c}`);
+  }
+};
 
-                    const topPad = Math.floor(heightDiff/2);
-                    const levelBuf = Buffer.alloc(256);
-                    const levelNumStr = totalLevels.toString();
-                    const paddedLevel2 = 
-                        [ ...Array.from(Array(topPad).keys()).map(()=>"".padStart(16, " ")),
-                        ...level.L.map((row) => (padStringLeft + row).padEnd(16)),
-                        ...Array.from(Array(heightDiff - topPad).keys()).map(()=>"".padStart(16, " ")) ].join("");
-                    const paddedLevel = writeNumberInMiddle(paddedLevel2, levelNumStr);
-                    for(let i = 0; i < paddedLevel.length; i++) {
-                        const c = paddedLevel[i];
-                        let outchar = 0;
-                        switch(c) {
-                            case '@':
-                            case 'p':
-                                outchar = 32;
-                                break;
-                            case '#':
-                                outchar = 166;
-                                break;
-                            case '.':
-                                outchar = 16;
-                                break;
-                            case '$':
-                                outchar = 252;
-                                break;
-                            case 'B':
-                            case '*':
-                                outchar = 253;
-                                break;
-                            case 'P':
-                            case '+':
-                                outchar = 24;
-                                break;
-                            case '0':
-                                outchar = 168;
-                                break;
-                            case '1':
-                                outchar = 169;
-                                break;
-                            case '2':
-                                outchar = 170;
-                                break;
-                            case '3':
-                                outchar = 171;
-                                break;
-                            case '4':
-                                outchar = 172;
-                                break;
-                            case '5':
-                                outchar = 173;
-                                break;
-                            case '6':
-                                outchar = 174;
-                                break;
-                            case '7':
-                                outchar = 175;
-                                break;
-                            case '8':
-                                outchar = 184;
-                                break;
-                            case '9':
-                                outchar = 185;
-                                break;
-                            default:
-                                outchar = (Math.random() > 0.75) ?
-                                Math.floor(Math.random() * 5) + 64 : 0;
-                        }
-                        levelBuf.writeUint8(outchar, i);
-                    }
-                    const sizeBuf = Buffer.alloc(1);
-                    const compressed = zlib.deflateRawSync(levelBuf);
-                    sizeBuf.writeUint8(compressed.length, 0);
-                    //console.log(paddedLevel);
-                    console.log(levelBuf.length + " -> " + compressed.length);
-                    totalSize += sizeBuf.length + compressed.length;
-                    return [sizeBuf, compressed];
-                }
-            }
-                return [];
-        });
-        fs.writeFileSync(outFileName,
-        Buffer.concat(convertedLevels));
-        console.log(totalSize);
-        console.log(`wrote ${convertedLevels.length / 2} levels to ${outFileName}`);
-    });
+function convertLevel(levelNum, level) {
+  const widthDiff = 16 - level.$.Width;
+  const heightDiff = 16 - level.$.Height;
+  const leftPad = Math.floor(widthDiff / 2);
+
+  const padStringLeft = "".padStart(leftPad, " ");
+
+  const topPad = Math.floor(heightDiff / 2);
+  const levelBuf = Buffer.alloc(256);
+
+  const paddedLevel = [
+    ...Array.from(range(topPad)).map(() => " ".repeat(16)),
+    ...level.L.map((row) => (padStringLeft + row).padEnd(16)),
+    ...Array.from(range(heightDiff - topPad)).map(() => " ".repeat(16)),
+  ].join("");
+
+  const levelWithNumber = writeLevelNumber(paddedLevel, levelNum.toString());
+
+  for (const [ix, c] of Array.from(levelWithNumber).entries()) {
+    levelBuf.writeUint8(getTileFromChar(c), ix);
+  }
+
+  return levelBuf;
+}
+
+function compressLevel(level) {
+  const sizeBuf = Buffer.alloc(1);
+  const compressed = zlib.deflateRawSync(level);
+  sizeBuf.writeUint8(compressed.length, 0);
+  console.log(level.length + " -> " + compressed.length);
+
+  return [sizeBuf, compressed];
+}
+
+// NOTE levels is a bit of a heterogenous array
+// the even numbered elements describe the size of the odd numbered elements
+function truncateLevels(levels, maxSize) {
+  let bytesRemaining = maxSize;
+  let ret = [];
+
+  for (let i = 0; i < levels.length / 2; i++) {
+    // NOTE +1 is because the size buffer itself one byte
+    const levelSize = levels[i * 2][0] + 1;
+
+    if (levelSize > bytesRemaining) {
+      return ret;
+    }
+
+    bytesRemaining -= levelSize;
+
+    ret.push(levels[i * 2]);
+    ret.push(levels[i * 2 + 1]);
+  }
+
+  return ret;
+}
+
+fs.readFile(inFileName, function (_err, data) {
+  parser.parseString(data, (_xmlErr, jsonObj) => {
+    const levels = jsonObj.SokobanLevels.LevelCollection[0].Level;
+
+    const convertedLevels = Array.from(
+      levels
+        .filter((level) => level.$.Width <= 16 && level.$.Height <= 14)
+        .entries()
+    ).map(([levelNum, level]) => convertLevel(levelNum, level));
+
+    let compressedLevels = convertedLevels.flatMap(compressLevel);
+    let bufToWrite = Buffer.concat(compressedLevels);
+
+    if (bufToWrite.byteLength > BANK_SIZE) {
+      console.warn("levels too big, truncating");
+
+      compressedLevels = truncateLevels(compressedLevels, BANK_SIZE);
+      bufToWrite = Buffer.concat(compressedLevels);
+    }
+
+    fs.writeFileSync(outFileName, bufToWrite);
+
+    console.log(
+      `wrote ${compressedLevels.length / 2} levels (${
+        bufToWrite.byteLength
+      } bytes) to ${outFileName}`
+    );
+  });
 });
