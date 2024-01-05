7848de3f053fbb78c6d5c10352bbc157cd7645f9
Clyde Shaffer <clydeshaffer@gmail.com>
2024-01-04 21:08:19 -0500

    demo for extracting single image directly to framebuffer

--- src/main.c
+++ src/main.c
@@ -1,39 +1,21 @@
 #include "gametank.h"
 #include "drawing_funcs.h"
+#include "gen/assets/art.h"
+#include <zlib.h>
+#include "gt/banking.h"
 
 int main () {
-    char col = 30, row = 20;
-    int dx = 1, dy = 1;
 
     init_graphics();
 
-    flip_pages();
-    clear_border(0);
-    await_draw_queue();
-    flip_pages();
-    await_draw_queue();
-    clear_border(0);
+    *dma_flags = DMA_CPU_TO_VRAM;
+    *bank_reg = 0;
+
+    change_rom_bank(ASSET__art__BeeshSpweeshNewYear_bmp_bank);
+    inflatemem(vram, &ASSET__art__BeeshSpweeshNewYear_bmp_ptr);
 
     while (1) {                                     //  Run forever
-        clear_screen(3);
-        draw_box(col, row, 8, 8, 92);
-        col += dx;
-        row += dy;
-        if(col == 1) {
-            dx = 1;
-        } else if(col == 119) {
-            dx = -1;
-        }
-        if(row == 8) {
-            dy = 1;
-        } else if(row == 112) {
-            dy = -1;
-        }
-        
-        await_draw_queue();
         sleep(1);
-        flip_pages();
-        
     }
 
   return (0);                                     //  We should never get here!
