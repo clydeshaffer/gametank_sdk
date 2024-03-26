034b3e5d0458b0807f882c0e20b27a46864c0b8e
Clyde Shaffer <clydeshaffer@gmail.com>
2024-03-26 16:31:56 -0400

    holy cow its working

---
+++ src/gt/persist.c
@@ -0,0 +1,74 @@
+#include "banking.h"
+#include "gametank.h"
+#include "drawing_funcs.h"
+#include "persist.h"
+
+#pragma code-name (push, "DATA")
+#pragma optimize (push, on)
+char i, k;
+void clear_save_sector() {
+    *dma_flags = flagsMirror & ~(DMA_IRQ | DMA_NMI);
+    asm("SEI");
+    change_rom_bank(SAVE_BANK_NUM);
+    //unlock
+    *((unsigned char*) 0x8AAA) = 0xAA;
+    *((unsigned char*) 0x8555) = 0x55;
+    //setup
+    *((unsigned char*) 0x8AAA) = 0x80;
+    
+    //unlock
+    *((unsigned char*) 0x8AAA) = 0xAA;
+    *((unsigned char*) 0x8555) = 0x55;
+
+    //erase
+    *((unsigned char*) 0x8000) = 0x30;
+
+    i = *((unsigned char*) 0x8000);
+    k = *((unsigned char*) 0x8000);
+    while(i != k) {
+        i = k;
+        k = *((unsigned char*) 0x8000);
+    }
+    *dma_flags = flagsMirror;
+    asm("CLI");
+}
+
+void unlock_bypass() {
+    *((unsigned char*) 0x8AAA) = 0xAA;
+    *((unsigned char*) 0x8555) = 0x55;
+    *((unsigned char*) 0x8AAA) = 0x20;
+}
+
+void lock_bypass() {
+    *((unsigned char*) 0x8000) = 0x90;
+    *((unsigned char*) 0x8000) = 0x00;
+}
+
+//src assumed not Flash ROM
+//dest assumned to be in Flash ROM
+void save_write(char* src, char* dest, char len) {
+    *dma_flags = flagsMirror & ~(DMA_IRQ | DMA_NMI);
+    asm("SEI");
+    change_rom_bank(SAVE_BANK_NUM);
+    i = 0;
+    k = len;
+    unlock_bypass();
+    while(k) {
+        *((unsigned char*) 0x8000) = 0xA0;
+        dest[i] = src[i];
+        while(dest[i] != src[i]) {
+            
+        }
+        i++;
+        k--;
+    }
+    lock_bypass();
+    *dma_flags = flagsMirror;
+    asm("CLI");
+}
+
+
+#pragma optimize (pop)
+#pragma code-name (pop)
+
+
--- src/main.c
+++ src/main.c
@@ -1,9 +1,16 @@
 #include "gametank.h"
 #include "drawing_funcs.h"
+#include "input.h"
+#include "persist.h"
+#include "banking.h"
+
+#pragma data-name (push, "SAVE")
+char saved_pos[4] = {30, 40, 1, 1};
+#pragma data-name (pop)
+
+char pos[4];
 
 int main () {
-    char col = 30, row = 20;
-    int dx = 1, dy = 1;
 
     init_graphics();
 
@@ -14,26 +21,38 @@ int main () {
     await_draw_queue();
     clear_border(0);
 
+
+    change_rom_bank(SAVE_BANK_NUM);
+    pos[0] = saved_pos[0];
+    pos[1] = saved_pos[1];
+    pos[2] = saved_pos[2];
+    pos[3] = saved_pos[3];
+
     while (1) {                                     //  Run forever
         clear_screen(3);
-        draw_box(col, row, 8, 8, 92);
-        col += dx;
-        row += dy;
-        if(col == 1) {
-            dx = 1;
-        } else if(col == 119) {
-            dx = -1;
+        draw_box(pos[1], pos[0], 8, 8, 92);
+        pos[1] += pos[2];
+        pos[0] += pos[3];
+        if(pos[1] == 1) {
+            pos[2] = 1;
+        } else if(pos[1] == 119) {
+            pos[2] = -1;
         }
-        if(row == 8) {
-            dy = 1;
-        } else if(row == 112) {
-            dy = -1;
+        if(pos[0] == 8) {
+            pos[3] = 1;
+        } else if(pos[0] == 112) {
+            pos[3] = -1;
         }
         
         await_draw_queue();
         sleep(1);
         flip_pages();
-        
+        update_inputs();
+        if(player1_buttons & ~player1_old_buttons & INPUT_MASK_A) {
+          clear_save_sector();
+          save_write(&pos, &saved_pos, sizeof(pos));
+        }
+
     }
 
   return (0);                                     //  We should never get here!
