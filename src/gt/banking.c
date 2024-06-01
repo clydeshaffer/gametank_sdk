9d31b8b8e3c3f2cb8dd694dd06502953214d0957
Clyde Shaffer <clydeshaffer@gmail.com>
2024-05-31 22:30:22 -0400

    port bank stack from ganymede

--- src/gt/banking.c
+++ src/gt/banking.c
@@ -1,30 +1,27 @@
 #include "gametank.h"
 
-unsigned char romBankMirror;
+//must be power of 2
+#define BANK_STACK_SIZE 8
+//must be BANK_STACK_SIZE-1
+#define BANK_WRAP_MASK 7
 
-void SwitchRomBank(unsigned char x);
+unsigned char romBankMirror;
+unsigned char romBankStack[BANK_STACK_SIZE];
+unsigned char romBankStackIdx;
+
+void bank_shift_out(unsigned char banknum);
 
 void change_rom_bank(unsigned char banknum) {
+    romBankStackIdx = (romBankStackIdx + 1) & BANK_WRAP_MASK;
+    romBankStack[romBankStackIdx] = romBankMirror;
     if(banknum == romBankMirror)
         return;
-    romBankMirror = banknum;
-    via[ORA] = 0;
-    via[ORA] = !!(banknum & 128) << 1;
-    via[ORA] |= 1;
-    via[ORA] = !!(banknum & 64) << 1;
-    via[ORA] |= 1;
-    via[ORA] = !!(banknum & 32) << 1;
-    via[ORA] |= 1;
-    via[ORA] = !!(banknum & 16) << 1;
-    via[ORA] |= 1;
-    via[ORA] = !!(banknum & 8) << 1;
-    via[ORA] |= 1;
-    via[ORA] = !!(banknum & 4) << 1;
-    via[ORA] |= 1;
-    via[ORA] = banknum & 2;
-    via[ORA] |= 1;
-    via[ORA] = !!(banknum & 1) << 1;
-    via[ORA] |= 1;
-    via[ORA] |= 4;
-    via[ORA] = 0;
+    bank_shift_out(banknum);
+}
+
+void pop_rom_bank() {
+    bank_shift_out(romBankStack[romBankStackIdx]);
+    if(romBankStackIdx == 0)
+        romBankStackIdx = BANK_STACK_SIZE;
+    --romBankStackIdx;
 }
