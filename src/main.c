fd131015cc5d77ee947b9cca825e6542ca480d34
Clyde Shaffer <clydeshaffer@gmail.com>
2024-07-16 20:21:27 -0400

    update and debug hardware test

--- src/main.c
+++ src/main.c
@@ -12,19 +12,48 @@
 int btn;
 char count;
 
+#define btn_check(a) \
+        btn = player##a##_buttons;\
+        if(btn & INPUT_MASK_UP) {\
+            --y##a;\
+        }\
+        if(btn & INPUT_MASK_DOWN) {\
+            ++y##a;\
+        }\
+        if(btn & INPUT_MASK_LEFT) {\
+            --x##a;\
+        }\
+        if(btn & INPUT_MASK_RIGHT) {\
+            ++x##a;\
+        }\
+        if(btn & INPUT_MASK_A) {\
+            c##a = 28;\
+        }\
+\
+        if(btn & INPUT_MASK_B) {\
+            c##a = 124;\
+        }\
+\
+        if(btn & INPUT_MASK_C) {\
+            c##a = 220;\
+        }\
+\
+        if(btn & INPUT_MASK_START) {\
+            c##a = 7;\
+        }\
+
 int main () {
-    register unsigned char frame = 0, subframe = 0, x = 32, y = 32, c = 92;
+    static unsigned char frame = 0, subframe = 0, x1 = 64, y1 = 32, c1 = 92, x2 = 64, y2 = 64, c2 = 92;
 
     init_graphics();
     init_dynawave();
     init_music();
 
-    flip_pages();
     clear_border(0);
     await_draw_queue();
     flip_pages();
-    await_draw_queue();
     clear_border(0);
+    await_draw_queue();
 
     load_big_spritesheet(&ASSET__character__jason, 1);
 
@@ -36,7 +65,7 @@ int main () {
         ++count;
         via[ORA] = count;
         clear_screen(3);
-        draw_box(x, y, 8, 8, c);
+        
         draw_sprite_frame(
             &ASSET__character__jason_json,
             40, 32, frame,
@@ -55,40 +84,15 @@ int main () {
             SPRITE_FLIP_Y, 1
         );
 
-        if(subframe & 1) {
-            btn = player1_buttons;
-        } else {
-            btn = player2_buttons;
-        }
-        
-        if(btn & INPUT_MASK_UP) {
-            --y;
-        }
-        if(btn & INPUT_MASK_DOWN) {
-            ++y;
-        }
-        if(btn & INPUT_MASK_LEFT) {
-            --x;
-        }
-        if(btn & INPUT_MASK_RIGHT) {
-            ++x;
-        }
+        draw_box(x1 & 127, y1 & 127, 8, 8, c1);
+        draw_box(x2 & 127, y2 & 127, 8, 8, c2);
 
-        if(btn & INPUT_MASK_A) {
-            c = 28;
-        }
+        c1 = 92;
+        c2 = 92;
 
-        if(btn & INPUT_MASK_B) {
-            c = 124;
-        }
+        btn_check(1);
 
-        if(btn & INPUT_MASK_C) {
-            c = 220;
-        }
-
-        if(btn & INPUT_MASK_START) {
-            c = 7;
-        }
+        btn_check(2);
 
         ++subframe;
         if(subframe == 8) {
