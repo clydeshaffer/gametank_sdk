06f0495e962efae003d15fa5edb723c8d605226a
Clyde Shaffer <clydeshaffer@gmail.com>
2024-10-22 21:54:09 -0400

    game loop working

--- src/main.c
+++ src/main.c
@@ -15,6 +15,7 @@ int audio_data_counter;
 #define CARD_COUNT 52
 #define DECK_SIZE 52
 #define START_HAND_SIZE 7
+#define RANK(x) (x & 0xF)
 char hand_size;
 char cpu_hand_size; //CPU hand counts down from CARD_COUNT - 1
 char cpu_hand_start_index;
@@ -106,17 +107,50 @@ void shuffle_deck() {
     }
 }
 
-#define STATE_NONE 0
+char count_rank_player_hand(char rank) {
+    static cnt, i;
+    cnt = 0;
+    for(i = 0; i < hand_size; ++i) {
+        if(RANK(card_num[i]) == rank) {
+            ++cnt;
+        }
+    }
+    return cnt;
+}
+
+char count_rank_cpu_hand(char rank) {
+    static cnt, i;
+    cnt = 0;
+    for(i = cpu_hand_start_index; i < CARD_COUNT; ++i) {
+        if(RANK(card_num[i]) == rank) {
+            ++cnt;
+        }
+    }
+    return cnt;
+}
+
+#define STATE_WAITING 0
 #define STATE_DEALING 1
 #define STATE_CPU_ASKING 2
 #define STATE_CPU_DRAWING 3
-#define STATE_PLAYER_GIVING 4
-#define STATE_PLAYER_ASKING 5
-#define STATE_CPU_GIVING 6
-#define STATE_PLAYER_DRAWING 7
+#define STATE_CPU_READING_CARD 4
+#define STATE_PLAYER_GIVING 5
+#define STATE_PLAYER_ASKING 6
+#define STATE_CPU_GIVING 7
+#define STATE_PLAYER_DRAWING 8
+#define STATE_CPU_SHOWING_CARD 9
+#define STATE_PLAYER_READING_CARD 10
 
-char select_cursor, cardgap, turn, dealing, wait_timer, selecting, requesting, requested;
+char select_cursor, cardgap, turn, dealing, wait_timer, selecting, cpu_requested_rank, num_cards_given;
+char player_selected_rank;
+char cpu_showing_index;
 char game_state, timeout_state;
+
+#define PICK_CPU_CARD_INDEX rnd_range(cpu_hand_start_index, CARD_COUNT)
+
+char pick_cpu_card_rank() {
+    return card_num[PICK_CPU_CARD_INDEX] & 0x0F;
+}
 
 int main () {
     static char i, nextcard;
@@ -157,12 +191,15 @@ int main () {
     dealing = START_HAND_SIZE;
     wait_timer = 30;
     selecting = 0;
-    requesting = 0xFF;
+    cpu_requested_rank = 0xFF;
 
     hand_size = 0;
     cpu_hand_size = 0;
     cpu_hand_start_index = CARD_COUNT;
     cardgap = 14;
+    num_cards_given = 0;
+    game_state = STATE_WAITING;
+    timeout_state = STATE_DEALING;
 
     for(i = 0; i < CARD_COUNT; ++i) {
         card_num[i] = 0xFF;
@@ -192,19 +229,6 @@ int main () {
                     push_audio_param(PITCH_MSB+i, 0);
                 }
                 flush_audio_params();
-                if(requesting != 0xFF) {
-                    set_voice_clip(requesting+1);
-                    requested = requesting;
-                    requesting = 0xFF;
-                } else if(requested != 0xFF) {
-                    selecting = 1;
-                }
-
-                if(last_audio_cursor == &ASSET__audiodata__awdangit_bin_ptr) {
-                    setclip(giveme);
-                    requesting = card_num[rnd_range(cpu_hand_start_index, CARD_COUNT)] & 0x0F;
-                    requested = 0xFF;
-                }
             }
         }
 
@@ -224,15 +248,6 @@ int main () {
 
         for(i = 0; i < CARD_COUNT; ++i) {
             if(card_num[i] != 0xFF) {
-                if(i < hand_size) {
-                    if(selecting) {
-                        if(i == select_cursor) {
-                            card_vy[i] = SELECT_HEIGHT;
-                        } else {
-                            card_vy[i] = UNSELECT_HEIGHT;
-                        }
-                    }
-                }
                 if(card_y[i] == 0) {
                     if(!(card_num[i] & 0x80)) {
                         card_num[i] |= 0x80;
@@ -261,51 +276,23 @@ int main () {
         }        
 
         clear_border(0);
-        
-        if(select_cursor < hand_size-1) {
-            if(player1_buttons & ~player1_old_buttons & INPUT_MASK_RIGHT) {
-                ++select_cursor;
-            }
-        }
-        if(select_cursor > 0) {
-            if(player1_buttons & ~player1_old_buttons & INPUT_MASK_LEFT) {
-                --select_cursor;
-            }
-        }
-
-        if(player1_buttons & ~player1_old_buttons & INPUT_MASK_A) {
-            //set_voice_clip((card_num[select_cursor] & 15) + 1);
-            if((card_num[select_cursor] & 15) == requested) {
-                card_x[hand_size] = card_x[select_cursor];
-                card_y[hand_size] = card_y[select_cursor];
-                card_vx[hand_size] = card_vx[select_cursor];
-                card_vy[hand_size] = 0;
-                card_num[hand_size] = card_num[select_cursor];
-                card_num[select_cursor] = 0xFF;
-                for(i = select_cursor; i < hand_size-1; ++i) {
-                    card_num[i] = card_num[i+1];
-                    card_x[i] = card_x[i+1];
-                    card_y[i] = card_y[i+1];
-                }
-                --hand_size;
-                card_num[hand_size] = 0xFF;
-                setclip(thankyou);
-                selecting = 0;
-            } else {
-                setclip(no);
-            }
-        }
-
-        if(player1_buttons & ~player1_old_buttons & INPUT_MASK_B) {
-            if(requested != 0xFF) {
-                setclip(awdangit);
-            }
-        }
 
         if(wait_timer) {
             --wait_timer;
-            if(!wait_timer) {
-                if(dealing) {
+        }
+
+        switch(game_state) {
+            case STATE_WAITING:
+                if(!wait_timer) {
+                    for(i = 0; i < hand_size; ++i) {
+                        select_cursor = 0;
+                        card_vy[i] = UNSELECT_HEIGHT;
+                    }
+                    game_state = timeout_state;
+                }
+                break;
+            case STATE_DEALING:
+                if(!wait_timer) {
                     card_x[hand_size] = 64;
                     card_y[hand_size] = 64;
                     card_num[hand_size] = deck[deck_index++];
@@ -321,10 +308,180 @@ int main () {
 
                     --dealing;
                     wait_timer = DEAL_ANIM_DELAY;
-                } else if (!selecting && !audio_data_counter && (requesting = 0xFF)) {
-                    setclip(giveme);
-                    requesting = card_num[rnd_range(cpu_hand_start_index, CARD_COUNT)] & 0x0F;
+                    if(!dealing) {
+                        game_state = STATE_WAITING;
+                        timeout_state = STATE_CPU_ASKING;
+                    }
                 }
+                break;
+            case STATE_CPU_ASKING:
+                if(timeout_state) {
+                    timeout_state = 0;
+                    setclip(giveme);
+                } else {
+                    if(audio_data_counter == 0) {
+                        if(last_audio_cursor == &ASSET__audiodata__giveme_bin_ptr) {
+                                cpu_requested_rank = pick_cpu_card_rank();
+                                set_voice_clip(cpu_requested_rank+1);
+                        } else {
+                            game_state = STATE_WAITING;
+                            timeout_state = STATE_PLAYER_GIVING;
+                            wait_timer = 30;
+                        }
+                    }
+                }
+                break;
+            case STATE_PLAYER_GIVING:
+                for(i = 0; i < hand_size; ++i) {
+                    if(i == select_cursor) {
+                        card_vy[i] = SELECT_HEIGHT;
+                    } else {
+                        card_vy[i] = UNSELECT_HEIGHT;
+                    }
+                }
+                if(player1_buttons & ~player1_old_buttons & INPUT_MASK_A) {
+                    if(RANK(card_num[select_cursor]) == cpu_requested_rank) {
+                        card_x[hand_size] = card_x[select_cursor];
+                        card_y[hand_size] = card_y[select_cursor];
+                        card_vx[hand_size] = card_vx[select_cursor];
+                        card_vy[hand_size] = 0;
+                        card_num[hand_size] = card_num[select_cursor];
+                        card_num[select_cursor] = 0xFF;
+                        for(i = select_cursor; i < hand_size-1; ++i) {
+                            card_num[i] = card_num[i+1];
+                            card_x[i] = card_x[i+1];
+                            card_y[i] = card_y[i+1];
+                        }
+                        --hand_size;
+                        ++num_cards_given;
+                        card_num[hand_size] = 0xFF;
+                    } else {
+                        setclip(no);
+                    }
+                } else if(player1_buttons & ~player1_old_buttons & INPUT_MASK_B) {
+                        if(cpu_requested_rank != 0xFF) {
+                            if(count_rank_player_hand(cpu_requested_rank)) {
+                                setclip(no);
+                            } else {
+                                if(!num_cards_given) {
+                                    setclip(awdangit);
+                                    game_state = STATE_WAITING;
+                                    wait_timer = 60;
+                                    timeout_state = STATE_CPU_DRAWING;
+                                } else {
+                                    num_cards_given = 0;
+                                    setclip(thankyou);
+                                    game_state = STATE_WAITING;
+                                    wait_timer = 60;
+                                    timeout_state = STATE_CPU_ASKING;
+                                }
+                            }
+                        }
+                    }
+                break;
+            case STATE_CPU_DRAWING:
+                --cpu_hand_start_index;
+                card_x[cpu_hand_start_index] = 64;
+                card_y[cpu_hand_start_index] = 64;
+                card_num[cpu_hand_start_index] = deck[deck_index++] | 0x80;
+                card_vy[cpu_hand_start_index] = 0;
+                ++cpu_hand_size;
+                game_state = STATE_WAITING;
+                wait_timer = 60;
+                timeout_state = STATE_CPU_READING_CARD;
+                break;
+            case STATE_CPU_READING_CARD:
+                if(RANK(card_num[cpu_hand_start_index]) == cpu_requested_rank) {
+                    setclip(haha);
+                    game_state = STATE_CPU_SHOWING_CARD;
+                    cpu_showing_index = cpu_hand_start_index;
+                    wait_timer = 60;
+                    timeout_state = STATE_CPU_ASKING;
+                } else {
+                    setclip(no);
+                    game_state = STATE_WAITING;
+                    wait_timer = 60;
+                    timeout_state = STATE_PLAYER_ASKING;
+                }
+                break;
+            case STATE_CPU_SHOWING_CARD:
+                card_vy[cpu_showing_index] = 26;
+                card_num[cpu_showing_index] &= 0x7F;
+                if(!wait_timer) {
+                    game_state = timeout_state;
+                    card_vy[cpu_showing_index] = 0;
+                    card_num[cpu_showing_index] |= 0x80;
+                }
+                break;
+            case STATE_PLAYER_ASKING:
+                for(i = 0; i < hand_size; ++i) {
+                    if(i == select_cursor) {
+                        card_vy[i] = SELECT_HEIGHT;
+                    } else {
+                        card_vy[i] = UNSELECT_HEIGHT;
+                    }
+                }
+                if(player1_buttons & ~player1_old_buttons & INPUT_MASK_A) {
+                    player_selected_rank = RANK(card_num[select_cursor]);
+                    if(count_rank_cpu_hand(player_selected_rank)) {
+                        setclip(awdangit);
+                        for(i = cpu_hand_start_index; i < CARD_COUNT; ++i) {
+                            if(RANK(card_num[i]) == player_selected_rank) {
+                                card_x[hand_size] = card_x[i];
+                                card_y[hand_size] = card_y[i]+1;
+                                card_num[hand_size] = card_num[i] & 0x7F;
+                                card_vy[hand_size] = UNSELECT_HEIGHT;
+                                ++hand_size;
+
+                                for(cpu_showing_index = i; cpu_showing_index < cpu_hand_start_index-2; ++cpu_showing_index) {
+                                    card_num[cpu_showing_index] = card_num[cpu_showing_index+1];
+                                    card_x[cpu_showing_index] = card_x[cpu_showing_index+1];
+                                    card_y[cpu_showing_index] = card_y[cpu_showing_index+1];
+                                }
+                                --cpu_hand_size;
+                                ++cpu_hand_start_index;
+                                card_num[cpu_hand_start_index-1] = 0xFF;
+                            }
+                        }
+                    } else {
+                        setclip(gofish);
+                        game_state = STATE_WAITING;
+                        wait_timer = 60;
+                        timeout_state = STATE_PLAYER_DRAWING;
+                    }
+                }
+                break;
+            case STATE_PLAYER_DRAWING:
+                    card_x[hand_size] = 64;
+                    card_y[hand_size] = 64;
+                    card_num[hand_size] = deck[deck_index++];
+                    card_vy[hand_size] = UNSELECT_HEIGHT;
+                    ++hand_size;
+                    game_state = STATE_WAITING;
+                    wait_timer = 60;
+                    timeout_state = STATE_PLAYER_READING_CARD;
+                    break;
+            case STATE_PLAYER_READING_CARD:
+                    if(RANK(card_num[hand_size-1]) == player_selected_rank) {
+                        game_state = STATE_PLAYER_ASKING;
+                    } else {
+                        game_state = STATE_WAITING;
+                        wait_timer = 60;
+                        timeout_state = STATE_CPU_ASKING;
+                    }
+                    break;
+            default:
+                break;
+        }
+        
+        if(select_cursor < hand_size-1) {
+            if(player1_buttons & ~player1_old_buttons & INPUT_MASK_RIGHT) {
+                ++select_cursor;
+            }
+        }
+        if(select_cursor > 0) {
+            if(player1_buttons & ~player1_old_buttons & INPUT_MASK_LEFT) {
+                --select_cursor;
             }
         }
 
