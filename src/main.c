305a74486484d896b23e9ecbb6061511be4df62b
Clyde Shaffer <clydeshaffer@gmail.com>
2024-12-14 15:29:48 -0500

    color test utility rom

--- src/main.c
+++ src/main.c
@@ -1,30 +1,24 @@
 #include "gt/gametank.h"
 #include "gt/gfx/draw_queue.h"
+#include "gt/gfx/draw_direct.h"
 
 char box_x = 30, box_y = 20;
 char dx = 1, dy = 1;
 
 int main () {
  
+
     while (1) {                                     //  Run forever
-        queue_clear_screen(3);
-        queue_draw_box(box_x, box_y, 8, 8, 92);
-        queue_clear_border(0);
-        
-        box_x += dx;
-        box_y += dy;
-        if(box_x == 1) {
-            dx = 1;
-        } else if(box_x == 119) {
-            dx = -1;
+        direct_prepare_box_mode();
+        dx = 0;
+        for(box_y = 0; box_y < 128; box_y += 4) {
+            for(box_x = 0; box_x < 128; box_x += 8) {
+                DIRECT_DRAW_COLOR(box_x, box_y, 8, 4, dx);
+                ++dx;
+                //await_drawing();
+            }
+            DIRECT_DRAW_COLOR(127, box_y, 1, 4, 0);
         }
-        if(box_y == 8) {
-            dy = 1;
-        } else if(box_y == 112) {
-            dy = -1;
-        }
- 
-        await_draw_queue();
         await_vsync(1);
         flip_pages();
  
