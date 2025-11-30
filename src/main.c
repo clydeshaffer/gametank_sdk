b306cb7ac1e84a14f4324e02c5adbb1880841da3
Clyde Shaffer <clydeshaffer@gmail.com>
2025-11-30 05:42:49 -0500

    add spikeball sprite and optimize routine

--- src/main.c
+++ src/main.c
@@ -1,19 +1,41 @@
 #include "gt/gametank.h"
 #include "gt/gfx/draw_queue.h"
 #include "gt/input.h"
+#include "gt/gfx/sprites.h"
+
+#include "gen/assets/sprites.h"
 
 char box_x = 30, box_y = 20;
 char dx = 1, dy = 1;
 char t = 0, tpl, tmi, ti;
 static char i;
-char scale;
+char scaleX, scaleY;
 
-signed char get_scaled_sine(char scale, char theta);
+extern char SineTheta;
+extern char SineOffset;
+signed char get_scaled_sine();
 
-extern signed char Sine256[256];
+extern unsigned char Sine256[256];
+
+SpriteSlot ballSpr;
+
+void set_box() {
+    SineTheta = ti;
+    SineOffset = scaleX;
+    box_x = get_scaled_sine() + 64;
+    SineTheta = ti+64;
+    SineOffset = scaleY;
+    box_y = get_scaled_sine() + 64;
+}
 
 void main () {
-    scale = 16;
+
+    ballSpr = allocate_sprite(&ASSET__sprites__spikeball_bmp_load_list);
+    set_sprite_frametable(ballSpr, &ASSET__sprites__spikeball_json);
+
+    scaleX = 64;
+    scaleY = 64;
+
     while (1) {                                     //  Run forever
         queue_clear_screen(3);
         update_inputs();
@@ -34,12 +56,14 @@ void main () {
 
         for(i = 0; i < 16; ++i) {
             ti = t + (i << 4);
-            tpl = ti + scale;
-            tmi = ti - scale;
-            box_x = ((Sine256[tpl] + Sine256[tmi]) >> 2) + 64;//get_scaled_sine(64, t) + 64;
-            box_y = ((Sine256[(tpl+64)%256] + Sine256[(tmi+64)%256]) >> 2) + 64;//get_scaled_sine(64, t) + 64;
+            //tpl = ti + scale;
+            //tmi = ti - scale;
+            //box_x = ((Sine256[tpl] + Sine256[tmi]) >> 2) + 64;//get_scaled_sine(64, t) + 64;
+            //box_y = ((Sine256[(tpl+64)%256] + Sine256[(tmi+64)%256]) >> 2) + 64;//get_scaled_sine(64, t) + 64;
+            set_box();
             //box_y = 64; //get_scaled_sine(64, t) + 64;
-            queue_draw_box(box_x-4, box_y-4, 8, 8, 60);
+            //queue_draw_box(box_x-4, box_y-4, 8, 8, 60);
+            queue_draw_sprite_frame(ballSpr, box_x, box_y, ((t >> 3) + i) & 15, 0);
         }
  
         queue_clear_border(0);
@@ -47,9 +71,11 @@ void main () {
         await_vsync(1);
         flip_pages();
 
-
-        if(player1_buttons & INPUT_MASK_UP) ++scale;
-        if(player1_buttons & INPUT_MASK_DOWN) --scale;
+        if(player1_buttons & INPUT_MASK_LEFT) ++scaleX;
+        if(player1_buttons & INPUT_MASK_RIGHT) --scaleX;
+        if(player1_buttons & INPUT_MASK_UP) ++scaleY;
+        if(player1_buttons & INPUT_MASK_DOWN) --scaleY;
+        //if(t & 1) ++scale;
 
         ++t;
     }
--- src/sine_scaler.s
+++ src/sine_scaler.s
@@ -1,66 +1,60 @@
-.export _get_scaled_sine, _Sine256
+.export _get_scaled_sine, _Sine256, _SineOffset, _SineTheta
 
 .zeropage
-SineTheta:
+_SineOffset:
 .res 1
-SineOffset:
+_SineTheta:
 .res 1
-SineAcc:
-.res 1
-.code
+
+.rodata
 
 _Sine256:
-    .byte $00,$03,$06,$09,$0C,$10,$13,$16
-    .byte $19,$1C,$1F,$22,$25,$28,$2B,$2E
-    .byte $31,$33,$36,$39,$3C,$3F,$41,$44
-    .byte $47,$49,$4C,$4E,$51,$53,$55,$58
-    .byte $5A,$5C,$5E,$60,$62,$64,$66,$68
-    .byte $6A,$6B,$6D,$6F,$70,$71,$73,$74
-    .byte $75,$76,$78,$79,$7A,$7A,$7B,$7C
-    .byte $7D,$7D,$7E,$7E,$7E,$7F,$7F,$7F
-    .byte $7F,$7F,$7F,$7F,$7E,$7E,$7E,$7D
-    .byte $7D,$7C,$7B,$7A,$7A,$79,$78,$76
-    .byte $75,$74,$73,$71,$70,$6F,$6D,$6B
-    .byte $6A,$68,$66,$64,$62,$60,$5E,$5C
-    .byte $5A,$58,$55,$53,$51,$4E,$4C,$49
-    .byte $47,$44,$41,$3F,$3C,$39,$36,$33
-    .byte $31,$2E,$2B,$28,$25,$22,$1F,$1C
-    .byte $19,$16,$13,$10,$0C,$09,$06,$03
-    .byte $00,$FD,$FA,$F7,$F4,$F0,$ED,$EA
-    .byte $E7,$E4,$E1,$DE,$DB,$D8,$D5,$D2
-    .byte $CF,$CD,$CA,$C7,$C4,$C1,$BF,$BC
-    .byte $B9,$B7,$B4,$B2,$AF,$AD,$AB,$A8
-    .byte $A6,$A4,$A2,$A0,$9E,$9C,$9A,$98
-    .byte $96,$95,$93,$91,$90,$8F,$8D,$8C
-    .byte $8B,$8A,$88,$87,$86,$86,$85,$84
-    .byte $83,$83,$82,$82,$82,$81,$81,$81
-    .byte $81,$81,$81,$81,$82,$82,$82,$83
-    .byte $83,$84,$85,$86,$86,$87,$88,$8A
-    .byte $8B,$8C,$8D,$8F,$90,$91,$93,$95
-    .byte $96,$98,$9A,$9C,$9E,$A0,$A2,$A4
-    .byte $A6,$A8,$AB,$AD,$AF,$B2,$B4,$B7
-    .byte $B9,$BC,$BF,$C1,$C4,$C7,$CA,$CD
-    .byte $CF,$D2,$D5,$D8,$DB,$DE,$E1,$E4
-    .byte $E7,$EA,$ED,$F0,$F4,$F7,$FA,$FD
+    .byte $00,$02,$03,$05,$06,$08,$09,$0B
+    .byte $0C,$0E,$10,$11,$13,$14,$16,$17
+    .byte $18,$1A,$1B,$1D,$1E,$20,$21,$22
+    .byte $24,$25,$26,$27,$29,$2A,$2B,$2C
+    .byte $2D,$2E,$2F,$30,$31,$32,$33,$34
+    .byte $35,$36,$37,$38,$38,$39,$3A,$3B
+    .byte $3B,$3C,$3C,$3D,$3D,$3E,$3E,$3E
+    .byte $3F,$3F,$3F,$40,$40,$40,$40,$40
+    .byte $40,$40,$40,$40,$40,$40,$3F,$3F
+    .byte $3F,$3E,$3E,$3E,$3D,$3D,$3C,$3C
+    .byte $3B,$3B,$3A,$39,$38,$38,$37,$36
+    .byte $35,$34,$33,$32,$31,$30,$2F,$2E
+    .byte $2D,$2C,$2B,$2A,$29,$27,$26,$25
+    .byte $24,$22,$21,$20,$1E,$1D,$1B,$1A
+    .byte $18,$17,$16,$14,$13,$11,$10,$0E
+    .byte $0C,$0B,$09,$08,$06,$05,$03,$02
+    .byte $00,$FE,$FD,$FB,$FA,$F8,$F7,$F5
+    .byte $F4,$F2,$F0,$EF,$ED,$EC,$EA,$E9
+    .byte $E8,$E6,$E5,$E3,$E2,$E0,$DF,$DE
+    .byte $DC,$DB,$DA,$D9,$D7,$D6,$D5,$D4
+    .byte $D3,$D2,$D1,$D0,$CF,$CE,$CD,$CC
+    .byte $CB,$CA,$C9,$C8,$C8,$C7,$C6,$C5
+    .byte $C5,$C4,$C4,$C3,$C3,$C2,$C2,$C2
+    .byte $C1,$C1,$C1,$C0,$C0,$C0,$C0,$C0
+    .byte $C0,$C0,$C0,$C0,$C0,$C0,$C1,$C1
+    .byte $C1,$C2,$C2,$C2,$C3,$C3,$C4,$C4
+    .byte $C5,$C5,$C6,$C7,$C8,$C8,$C9,$CA
+    .byte $CB,$CC,$CD,$CE,$CF,$D0,$D1,$D2
+    .byte $D3,$D4,$D5,$D6,$D7,$D9,$DA,$DB
+    .byte $DC,$DE,$DF,$E0,$E2,$E3,$E5,$E6
+    .byte $E8,$E9,$EA,$EC,$ED,$EF,$F0,$F2
+    .byte $F4,$F5,$F7,$F8,$FA,$FB,$FD,$FE
+.code
 
 .proc _get_scaled_sine: near
-    STA SineTheta
-    STX SineOffset
-
     CLC
-    ADC SineTheta
+    LDA _SineTheta
+    ADC _SineOffset
     TAX
-    LDA _Sine256, x
-    STA SineAcc
-
-    LDA SineTheta
     SEC
-    SBC SineOffset
-    TAX
-    LDA _Sine256, x
-    
+    LDA _SineTheta
+    SBC _SineOffset
+    TAY
     CLC
-    ADC SineAcc
+    LDA _Sine256, x
+    ADC _Sine256, y
     LDX #0
     RTS
 .endproc
